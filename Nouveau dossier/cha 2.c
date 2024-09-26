b066add8566cb4048ff6944e9e9a7e7cdd3f9265
kenza bouzouida <yacinemarzou@gmail.com>
2024-09-26 19:20:10 +0100

    firs

---
+++ Nouveau dossier/cha 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float c,k;
+
+    printf("la  temperature en celsius :\n");
+    scanf("%f",&c);
+    k=c+273.15;
+    printf("la temperature la transforme en Kelvin:%f",k);
+
+
+   return 0;
+
+}
